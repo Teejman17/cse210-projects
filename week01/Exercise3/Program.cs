7434e3a08ea579c282fbe61f2fd88dbd31ad30c2
Teejman17 <tmccoy@byupathway.edu>
2025-05-12 00:27:04 -0600

    Week 1 exercieses

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Console.WriteLine("What is your number? ");
+        // string Console.ReadLine();
+        // int number = int.Parse(Console.ReadLine());
+
+         Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess = 0;
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+        }
     }
 }
